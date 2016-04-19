c4aa4823854a9d073b805c4854ad9c9b86b5e190
Poss111 <dpatposs@msn.com>
2016-04-19 11:37:20 -0600

    Added step4 files and grading script

---
+++ GradeStep4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INPUTS=Step4/inputs/*
+mkdir Step4/usertest
+for i in $INPUTS
+	do
+		filename=${i%.*}
+		name=${filename##*/}
+		echo "Testing input file $i"
+		output="${name}Test.out"
+		outtest="${name}.out"
+		./Micro $i > Step4/usertest/$output
+		diff -b -s Step4/usertest/$output Step4/outputs/$outtest
+	done
